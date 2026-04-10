833588a4212e4e31f2627c9720454ce30c0f81b3
ramptdr <ramptdr523288@gmail.com>
2026-04-10 08:36:48 +0530

    "read logic added"

--- axi_master.v
+++ axi_master.v
@@ -20,4 +20,60 @@ module axi_master (
 
 );
 
+
+
+assign axi_stall = (state != IDLE) || ((cpu_mem_read || cpu_mem_write) && state == IDLE);
+localparam IDLE      = 2'b00; // doing nothing waiting for the cpu to send address
+localparam READ_ADDR = 2'b01; // Sending Address
+localparam READ_DATA = 2'b10; // Waiting for Data
+
+reg [1:0] state;
+
+always @(posedge clk) begin 
+
+if(reset) begin 
+  state<=IDLE;
+  m_axi_arvalid<=1'b0; // Stop shouting "I have an address"
+  m_axi_rready<=1'b0; // Close your hands (not ready for data yet)
+   m_axi_araddr<=32'b0;  // Clear the address pins
+  cpu_mem_rdata<=32'b0; // Clear the data returned to CPU 
+
+ 
+  
+
+
+
+end 
+else begin
+    case (state) 
+
+    IDLE: begin 
+        m_axi_rready <= 0;
+        if(cpu_mem_read) begin 
+            m_axi_araddr  <= cpu_mem_addr;
+            m_axi_arvalid <= 1'b1;
+            state         <= READ_ADDR;
+        end
+    end
+
+    READ_ADDR: begin
+        if(m_axi_arvalid && m_axi_arready) begin 
+            m_axi_arvalid <= 0;
+            m_axi_rready  <= 1'b1;
+            state         <= READ_DATA;
+        end
+    end
+
+    READ_DATA: begin 
+        if(m_axi_rvalid && m_axi_rready) begin
+            cpu_mem_rdata <= m_axi_rdata;
+            m_axi_rready  <= 0;
+            state         <= IDLE;
+        end
+    end 
+
+    endcase
+end
+end 
+
 endmodule
