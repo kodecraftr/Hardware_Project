128f5d94e6fa54c80b2f86e0cfdc6d25ce6d0220
ramptdr <ramptdr523288@gmail.com>
2026-04-10 11:58:40 +0530

    "axi master done"

--- axi_master.v
+++ axi_master.v
@@ -16,20 +16,35 @@ module axi_master (
     //AXI Read Data Channel (The "Catching" wires)
     input  wire [31:0] m_axi_rdata,  // The actual package (data)
     input  wire        m_axi_rvalid, // The Slave's "Here it is!"
-    output reg         m_axi_rready  // Your "My hands are open"
+    output reg         m_axi_rready,  // Your "My hands are open"
 
+    //AXI Write Address Channel (AW) "Where"
+    output reg [31:0] m_axi_awaddr, // This carries the destination address from the CPU.
+    output reg        m_axi_awvalid, // You raise this to say, "I am now driving a valid write address."
+    input  wire       m_axi_awready, // The Slave raises this to say, "I have captured the address."
+
+    //AXI Write Data Channel (W) "What"
+    output reg [31:0] m_axi_wdata, // This carries the actual data the CPU wants to store (cpu_mem_wdata).
+    output reg        m_axi_wvalid, // You raise this to say, "The data on the bus is the real value to be saved."
+    input  wire       m_axi_wready, // The Slave raises this to say, "I have received the data."
+
+    //AXI Write Response Channel (B) "Result"
+    input  wire       m_axi_bvalid, // The Slave raises this to say, "The write to the actual memory hardware is finished and successful."
+    output reg        m_axi_bready // You raise this to say, "I am ready to hear your success message."
 );
 
 
 
 assign axi_stall = (state != IDLE) || ((cpu_mem_read || cpu_mem_write) && state == IDLE);
-localparam IDLE      = 2'b00; // doing nothing waiting for the cpu to send address
-localparam READ_ADDR = 2'b01; // Sending Address
-localparam READ_DATA = 2'b10; // Waiting for Data
-
-reg [1:0] state;
-
+localparam IDLE      = 3'b000; // doing nothing waiting for the cpu to send address
+localparam READ_ADDR = 3'b001; // Sending Address
+localparam READ_DATA = 3'b010; // Waiting for Data
+localparam WRITE_DATA = 3'b011; // Sending Address and Data
+localparam WRITE_RESP = 3'b100; // Waiting for B-channel response
+reg [2:0] state;
+reg aw_done,w_done;
 always @(posedge clk) begin 
+
 
 if(reset) begin 
   state<=IDLE;
@@ -37,23 +52,34 @@ if(reset) begin
   m_axi_rready<=1'b0; // Close your hands (not ready for data yet)
    m_axi_araddr<=32'b0;  // Clear the address pins
   cpu_mem_rdata<=32'b0; // Clear the data returned to CPU 
-
- 
-  
-
-
-
-end 
+    m_axi_awvalid <= 1'b0;
+    m_axi_wvalid  <= 1'b0;
+    m_axi_bready  <= 1'b0;
+    aw_done<=1'b0;
+    w_done<=1'b0;
+ end 
 else begin
     case (state) 
 
     IDLE: begin 
         m_axi_rready <= 0;
-        if(cpu_mem_read) begin 
+      //  m_axi_wready <=0;
+        if(cpu_mem_read&&!cpu_mem_write) begin 
             m_axi_araddr  <= cpu_mem_addr;
             m_axi_arvalid <= 1'b1;
             state         <= READ_ADDR;
         end
+        else if(cpu_mem_write) begin 
+             m_axi_awvalid<=1'b1; // valid write address
+              m_axi_wvalid <=1'b1; // real valued data is given 
+               m_axi_awaddr<= cpu_mem_addr; // address where data is to be stored
+               m_axi_wdata <=cpu_mem_wdata; // data that need to be stored
+               aw_done <=1'b0; // reset trackers
+               w_done <= 1'b0; // reset trackers
+               state <= WRITE_DATA; // move the handshake phase
+
+            end
+
     end
 
     READ_ADDR: begin
@@ -71,7 +97,29 @@ else begin
             state         <= IDLE;
         end
     end 
+    WRITE_DATA : begin 
+        if(m_axi_awvalid&& m_axi_awready) begin 
+            m_axi_awvalid <=1'b0;
+            aw_done<=1'b1;
+        end
+        if(m_axi_wvalid && m_axi_wready) begin 
+            m_axi_wvalid<=1'b0;
+            w_done<=1'b1;
+        end
+            // Check if both channels have finished their handshake
+        if (aw_done && w_done) begin
+        state        <= WRITE_RESP;
+        m_axi_bready <= 1'b1; // Start listening for the response
+            
+        end
 
+    end
+    WRITE_RESP : begin
+        if(m_axi_bvalid&&m_axi_bready) begin 
+            m_axi_bready <=1'b0;
+            state <=IDLE; // write is complete
+        end
+     end
     endcase
 end
 end 
